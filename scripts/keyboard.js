fda0d972df84d186f85431e423d84c74057bbf5c
Eugene Helland <eugenehelland@mail.com>
2021-12-10 17:10:58 -0700

    Simulate button pushes on screen in concert with physical key pressed.

--- scripts/keyboard.js
+++ scripts/keyboard.js
@@ -13,8 +13,41 @@ function translateOperand(keyPress) {
   return translated;
 }
 
+function pressed(keyPress) {
+  const values = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    0: 'zero',
+    '.': 'dot',
+    Enter: 'equals',
+    '=': 'equals',
+    Backspace: 'backspace',
+    Delete: 'clear',
+    '/': 'division',
+    '*': 'multiplication',
+    '-': 'subtraction',
+    '+': 'addition',
+  };
+
+  const className = values[keyPress];
+
+  const key = document.querySelector(`.${className}`);
+  key.classList.add('pressed');
+  setTimeout(() => {
+    key.classList.remove('pressed');
+  }, 100);
+}
+
 function keyboardEvent(e) {
   const keyPress = e.key;
+  pressed(keyPress);
 
   if (!isNaN(keyPress)) {
     numPadDisplay(keyPress);
